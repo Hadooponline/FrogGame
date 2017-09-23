4eb984ca3ef8f79cf733af8f110db0f17e998a26
YoranMa <349316972@qq.com>
2017-09-23 16:43:53 +0800

    Update app.js
    主要优化功能点：
    1.碰撞范围扩大，使碰撞看起来更精准。
    2.定义了角色类及其位置属性、头像属性，及render函数，让虫子和万家分别继承。
    3.获胜标志、玩家活动范围边界值及玩家移动时上下距离或左右距离值改为全局设置。

--- js/app.js
+++ js/app.js
@@ -1,90 +1,99 @@
 "use strict";
-// 这是我们的玩家要躲避的敌人 
-var Enemy = function(x,y,speed) {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
+//角色类
+var Character = function(x,y,sprite){
     this.x = x;
     this.y = y;
-    this.speed=speed;
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = sprite;
 };
-
-// 此为游戏必须的函数，用来更新敌人的位置
-// 参数: dt ，表示时间间隙
-Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
-    if(player.won==false){
-        this.x = this.x + this.speed*dt;
-        if(this.x>505) this.x=-101;
-        this.checkCollisions();    
-    }
-};
-
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
-Enemy.prototype.render = function() {    
+//此为游戏必须的函数，用来画出屏幕上的角色
+Character.prototype.render=function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-//敌人和玩家遭遇，游戏重新开始
-Enemy.prototype.checkCollisions = function(){
-     if(player.x>this.x-50&&player.x<this.x+30)
-        if(player.y>this.y-50&&player.y<this.y+30){
-          game.replay();
-        }
-};
-
-// 现在实现你自己的玩家类
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-var Player = function(x,y){
-    //玩家赢了标志
-    this.won=false;
-    //玩家位置
-    this.x = x;
-    this.y = y;
-    this.speed=20;
-    //玩家活动边界
-    this.borderleft=0;
-    this.borderright=400;
-    this.bordertop=0;
-    this.borderbottom=400;
-    this.sprite = 'images/char-pink-girl.png';
-};
-
-Player.prototype.update = function(dt){
-    
-};
-Player.prototype.render = function(){
-    //玩家顺利抵达河流边
-   if(this.y==0&&this.won==false){
-        this.won=true;
-    }
     //玩家获胜
-    if(this.won==true){
+    if(Won==true){
         ctx.drawImage(Resources.get('images/Star.png'),210,200);
         ctx.drawImage(Resources.get('images/Star.png'),110,200);
         ctx.drawImage(Resources.get('images/Star.png'),310,200);
 
         game.showBtnStart();
     }
-
-    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
 };
+
+// 这是我们的玩家要躲避的敌人 
+var Enemy = function(x,y,speed) {
+    // 要应用到每个敌人的实例的变量写在这里
+    // 我们已经提供了一个来帮助你实现更多
+    var enemy = Object.create(Enemy.prototype);
+    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    Character.call(enemy,x,y,'images/enemy-bug.png');
+    enemy.speed = speed;
+    return enemy;
+};
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
+
+// 此为游戏必须的函数，用来更新敌人的位置
+// 参数: dt ，表示时间间隙
+Enemy.prototype.update = function(dt) {
+    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 都是以同样的速度运行的
+    if(Won==false){
+        this.x = this.x + this.speed*dt;
+        if(this.x>505) this.x=-101;
+        this.checkCollisions();    
+    }
+};
+
+//敌人和玩家遭遇，游戏重新开始
+Enemy.prototype.checkCollisions = function(){
+     if(Math.abs(player.x-this.x)<70 && Math.abs(player.y-this.y)<70){
+          game.replay();
+     }
+};
+
+
+// 现在实现你自己的玩家类
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+var Player = function(x,y){
+    var player = Object.create(Player.prototype);
+    Character.call(player,x,y,'images/char-pink-girl.png');
+    //玩家位置
+    return player;
+};
+
+Player.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function(dt){
+    if(this.y==0&&Won==false){
+        Won=true;
+    }    
+};
+
+//玩家赢了标志
+var Won=false;
+//玩家活动边界
+var BorderLeft=0;
+var BorderRight=400;
+var BorderTop=0;
+var BorderBottom=400;
+var PlayerStepWidth=101;
+var PlayerStepHeigth=83;
+
 //玩家依靠键盘移动位置
 Player.prototype.handleInput = function(direction){
-    if(this.won==false){
+    if(Won==false){
          switch(direction){
             case "left":
-                this.x = (this.x-this.speed)<this.borderleft?this.borderleft:this.x-this.speed;
+                this.x = (this.x-PlayerStepWidth)<BorderLeft?BorderLeft:this.x-PlayerStepWidth;
                 break;
             case "right":
-                this.x = (this.x+this.speed)>this.borderright?this.borderright:this.x+this.speed;
+                this.x = (this.x+PlayerStepHeigth)>BorderRight?BorderRight:this.x+PlayerStepHeigth;
                 break;
             case "up":
-                this.y = (this.y-this.speed)<this.bordertop?this.bordertop:this.y-this.speed;
+                this.y = (this.y-PlayerStepHeigth)<BorderTop?BorderTop:this.y-PlayerStepHeigth;
                 break;
             case "down":
-                this.y = (this.y+this.speed)>this.borderbottom?this.borderbottom:this.y+this.speed;
+                this.y = (this.y+PlayerStepWidth)>BorderBottom?BorderBottom:this.y+PlayerStepWidth;
                 break;
         }
     }   
@@ -138,7 +147,7 @@ document.addEventListener('keyup', function(e) {
 Game.prototype.resetPlayer = function(){
    player.x=200;
    player.y=400;
-   player.won=false;
+   Won=false;
 };
 
 //
